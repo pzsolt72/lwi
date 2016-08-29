211d89980fc237e38f99abc71394b06318648ea0
Peter Szilagyi <peter.szilagyi@gmail.com>
2016-08-29 20:37:22 +0200

    [REQ-LWI-005] security handler refactor

--- wfplugins/src/main/java/hu/telekom/lwi/plugin/security/SecurityHandler.java
+++ wfplugins/src/main/java/hu/telekom/lwi/plugin/security/SecurityHandler.java
@@ -1,7 +1,10 @@
 package hu.telekom.lwi.plugin.security;
 
+import java.security.Principal;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.jboss.logging.Logger;
 
@@ -11,13 +14,15 @@ import io.undertow.security.handlers.AuthenticationCallHandler;
 import io.undertow.security.handlers.AuthenticationConstraintHandler;
 import io.undertow.security.handlers.AuthenticationMechanismsHandler;
 import io.undertow.security.handlers.SecurityInitialHandler;
+import io.undertow.security.idm.Account;
+import io.undertow.security.idm.Credential;
+import io.undertow.security.idm.IdentityManager;
 import io.undertow.security.impl.BasicAuthenticationMechanism;
 import io.undertow.security.impl.ClientCertAuthenticationMechanism;
-import io.undertow.security.impl.DigestAuthenticationMechanism;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 
-public class SecurityHandler implements HttpHandler {
+public class SecurityHandler implements HttpHandler, IdentityManager {
 	
 	private static final Logger log = Logger.getLogger(SecurityHandler.class);
 	
@@ -32,13 +37,13 @@ public class SecurityHandler implements HttpHandler {
 		List<AuthenticationMechanism> mechanisms = new ArrayList<AuthenticationMechanism>();
 		
 		mechanisms.add(new BasicAuthenticationMechanism(REALM));
-		mechanisms.add(new DigestAuthenticationMechanism(REALM, REALM, "DIGEST"));
+//		mechanisms.add(new DigestAuthenticationMechanism(REALM, REALM, "DIGEST"));
 		mechanisms.add(new ClientCertAuthenticationMechanism());
-		
+
 		authenticationCallHandler = new AuthenticationCallHandler(next);
 		mechanismsHandler = new AuthenticationMechanismsHandler(authenticationCallHandler, mechanisms);
 		constraintHandler = new AuthenticationConstraintHandler(mechanismsHandler);
-		initialHandler = new SecurityInitialHandler(AuthenticationMode.CONSTRAINT_DRIVEN, new LwiIdentityManager(), constraintHandler);
+		initialHandler = new SecurityInitialHandler(AuthenticationMode.CONSTRAINT_DRIVEN, this, constraintHandler);
 	}
 	
 	@Override
@@ -46,4 +51,41 @@ public class SecurityHandler implements HttpHandler {
 		log.info(String.format("SecurityHandler.handleRequest invoked"));
 		initialHandler.handleRequest(exchange);
 	}
+
+	@Override
+	public Account verify(Account account) {
+		log.info("SecurityHandler.verify invoked - "+account);
+		return account;
+	}
+
+	@Override
+	public Account verify(final String id, final Credential credential) {
+		log.info("SecurityHandler.verify invoked - "+id+" - "+credential);
+		
+		return new Account() {
+			@Override
+			public Set<String> getRoles() {
+				Set<String> roles = new HashSet<String>();
+				roles.add("LWI_ROLE");
+				return roles;
+			}
+			
+			@Override
+			public Principal getPrincipal() {
+				return new Principal() {
+					@Override
+					public String getName() {
+						return id;
+					}
+				};
+			}
+		};
+	}
+
+	@Override
+	public Account verify(Credential credential) {
+		log.info("SecurityHandler.verify invoked - "+credential);
+		return verify("LWI_ADMIN", credential);
+	}
+
 }
