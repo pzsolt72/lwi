14201617b3fe35adee954934bc34ef8a077eaa0a
Peter Szilagyi <peter.szilagyi@gmail.com>
2016-09-17 00:18:57 +0200

    [REQ-LWI-003] refactored logger
    * separated logic from conduits and wrapper
    * general way of handling common parts in log

--- lwi-wildflyplugin/src/main/java/hu/telekom/lwi/plugin/log/LwiLogHandler.java
+++ lwi-wildflyplugin/src/main/java/hu/telekom/lwi/plugin/log/LwiLogHandler.java
@@ -1,22 +1,13 @@
 package hu.telekom.lwi.plugin.log;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.jboss.logging.Logger;
 
 import hu.telekom.lwi.plugin.LwiHandler;
-import hu.telekom.lwi.plugin.util.LwiLogAttributeUtil;
-import io.undertow.server.ExchangeCompletionListener;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 
 public class LwiLogHandler implements HttpHandler {
 
-	private static final String REQUEST_LOG_MAIN = "[%s][%s][%s > %s.%s]";
-	private static final String RESPONSE_LOG_MAIN = "[%s][%s][%s < %s.%s]";
-	protected static final String CTX_LOG = "[RequestId: %s CorrelationId: %s UserId: %s]";
-	
 	private HttpHandler next = null;
 
 	private final Logger log = Logger.getLogger(this.getClass());
@@ -34,94 +25,30 @@ public class LwiLogHandler implements HttpHandler {
 		String lwiRequestId = LwiHandler.getLwiRequestId(exchange);
 		
 		log.info(String.format("[%s] LwiLogHandler > start request/response log handling (%s)...", lwiRequestId, logLevel.name()));
-
-		String[] requestPath = exchange.getRequestPath().split("/");
 		
-		String caller = LwiLogAttribute.EMPTY;
-		if (exchange.getSecurityContext() != null && exchange.getSecurityContext().getAuthenticatedAccount() != null && exchange.getSecurityContext().getAuthenticatedAccount().getPrincipal() != null) {
-			caller = exchange.getSecurityContext().getAuthenticatedAccount().getPrincipal().getName();
-		}
+		LwiCall lwiCall = new LwiCall(exchange, lwiRequestId);
+		LwiRequestData lwiRequestData = null;
 
-		final int firstElementOnPath = 1; // set which part of the url should be parsed as provider and operation
-		String provider = LwiLogAttribute.EMPTY;
-		String operation = LwiLogAttribute.EMPTY;
-		for (int i = firstElementOnPath; i < requestPath.length; i++) {
-			if (i >= requestPath.length - 1) {
-				operation = requestPath[i];
-			} else if (i == firstElementOnPath) {
-				provider = requestPath[i];
-			} else {
-				provider += "."+requestPath[i];
-			}
-		}
-
-		boolean infoFromHeaders = true;
-		
-		long startTimeInMillis = System.currentTimeMillis();
-
-		StringBuilder requestLogMessage = new StringBuilder(String.format(REQUEST_LOG_MAIN, lwiRequestId, getTimestamp(startTimeInMillis), caller, provider, operation));
-		StringBuilder responseLogMessage = new StringBuilder(String.format(RESPONSE_LOG_MAIN, lwiRequestId, "%s", caller, provider, operation));
-		
 		if (logLevel != LwiLogLevel.MIN) {
-			
-			String requestId = LwiLogAttributeUtil.getHttpHeaderAttribute(exchange, LwiLogAttribute.RequestId);
-			String correlationId = LwiLogAttributeUtil.getHttpHeaderAttribute(exchange, LwiLogAttribute.CorrelationId);
-			String userId = LwiLogAttributeUtil.getHttpHeaderAttribute(exchange, LwiLogAttribute.UserId);
-
-			if (requestId != null && correlationId != null && userId != null) {
-				infoFromHeaders = true;
-				requestLogMessage.append(String.format(CTX_LOG, requestId, correlationId, userId));
-				responseLogMessage.append(String.format(CTX_LOG, requestId, correlationId, userId));
-			} else {
-				infoFromHeaders = false;
-			}
+			lwiRequestData = new LwiRequestData(exchange);
 		}
 
-		final LwiConduitWrapper conduitHandler;
-		
-		if (logLevel == LwiLogLevel.FULL || !infoFromHeaders) {
-			conduitHandler = new LwiConduitWrapper(messageLog, logLevel, requestLogMessage.toString(), responseLogMessage.toString(), !infoFromHeaders, startTimeInMillis);
-			
-			exchange.addRequestWrapper(conduitHandler.getRequestConduit());
-			exchange.addResponseWrapper(conduitHandler.getResponseConduit());
-		} else {
-			conduitHandler = null;
+		final LwiConduitWrapper conduitHandler = new LwiConduitWrapper(messageLog, lwiCall, lwiRequestData, logLevel == LwiLogLevel.FULL);
 
-			messageLog.info(requestLogMessage);
-		}
+		conduitHandler.applyConduits(exchange);
 
-		exchange.addExchangeCompleteListener(new ExchangeCompletionListener() {
-			@Override
-			public void exchangeEvent(final HttpServerExchange exchange, final NextListener nextListener) {
+		exchange.addExchangeCompleteListener(conduitHandler);
 
-				if (conduitHandler != null) {
-					conduitHandler.log(false);
-				} else {
-					messageLog.info(String.format(responseLogMessage.toString(), getTimestamp()));
-				}
-
-				nextListener.proceed();
-
-				log.debug(String.format("[%s] LwiLogHandler > handle complete!", lwiRequestId));
-
-			}
-		});
-
+		lwiCall.setRequestFinished();
 		next.handleRequest(exchange);
+		lwiCall.setResponseStarted();
+
+		log.info(String.format("[%s] LwiLogHandler > handle complete!", lwiRequestId));
 	}
 	
 	
 	public void setLogLevel(String logLevel) {
 		this.logLevel = LwiLogLevel.valueOf(logLevel);
-	}
-
-	public static String getTimestamp() {
-		return getTimestamp(System.currentTimeMillis());
-	}
-
-	public static String getTimestamp(long timeInMillis) {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS");
-		return sdf.format(new Date(timeInMillis));
 	}
 
 }
---
+++ lwi-wildflyplugin/src/main/java/hu/telekom/lwi/plugin/log/LwiRequestData.java
@@ -0,0 +1,52 @@
+package hu.telekom.lwi.plugin.log;
+
+import hu.telekom.lwi.plugin.util.LwiLogAttributeUtil;
+import io.undertow.server.HttpServerExchange;
+
+public class LwiRequestData {
+	
+	protected static final String LOG_FORMAT = "[RequestId: %s CorrelationId: %s UserId: %s]";
+
+	private String requestId;
+	private String correlationId;
+	private String userId;
+
+	public LwiRequestData(HttpServerExchange exchange) {
+		requestId = LwiLogAttributeUtil.getHttpHeaderAttribute(exchange, LwiLogAttribute.RequestId);
+		correlationId = LwiLogAttributeUtil.getHttpHeaderAttribute(exchange, LwiLogAttribute.CorrelationId);
+		userId = LwiLogAttributeUtil.getHttpHeaderAttribute(exchange, LwiLogAttribute.UserId);
+	}
+
+	public boolean isNullRequestId() {
+		return requestId == null;
+	}
+
+	public boolean isNullCorrelationId() {
+		return correlationId == null;
+	}
+
+	public boolean isNullUserId() {
+		return userId == null;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public void setCorrelationId(String correlationId) {
+		this.correlationId = correlationId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public boolean parseRequestRequired() {
+		return isNullRequestId() || isNullCorrelationId() || isNullUserId();
+	}
+	
+	public String toLog() {
+		return String.format(LOG_FORMAT, requestId!=null?requestId:LwiLogAttribute.EMPTY, correlationId!=null?correlationId:LwiLogAttribute.EMPTY, userId!=null?userId:LwiLogAttribute.EMPTY);
+	}
+
+}
