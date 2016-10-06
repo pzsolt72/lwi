3c85f698ca9415c8dcdd0db28b14901274d82d83
gyenes2zolt531-dvr <gyenes.zoltan@gmail.com>
2016-10-06 10:26:37 +0200

    config generátor update + server.yml kiegészítése

--- lwi-docker/script/lwi_create_config.pl
+++ lwi-docker/script/lwi_create_config.pl
@@ -20,6 +20,10 @@ my $REALM="ApplicationRealm";
 my %USERS = ();
 my %USER_CONSUMED_SERVICES = ();
 my @CLI = ();
+my $JKS_PASSWD;
+my $JKS_PATH;
+my %HOSTS = ();
+
 push @CLI, "# run with: ./jboss-cli.sh --commands=embed-server,run-batch\ --file=lwi.cli";
 push @CLI, '# logger config';
 push @CLI, '/subsystem=logging/size-rotating-file-handler=LWI_LOG_MESSAGE:add(autoflush=true,file={"path"=>"/home/dockeri/logs/lwi_audit.log"},append=true,suffix=.yyyy-MM-dd,formatter="%d{HH:mm:ss,SSS} %-5p [%c] (%t) %s%E%n",level="INFO",rotate-size="100M",max-backup-index="10")';
@@ -46,6 +50,7 @@ my @serviceDatFiles = glob "$inServiceDatFileMask";
 checkInputFiles();
 
 readUsersDatFile();
+readServerDatFile();
 
 foreach my $serviceDatFile (@serviceDatFiles) {
   debugMsg("Service file: $serviceDatFile");
@@ -90,10 +95,37 @@ sub readUsersDatFile {
   }
 }
 
+sub readServerDatFile {
+  my($datFile);
+  my($section) = "";
+  open($datFile, $inServerDatFile) || die "Cannot open $inServerDatFile\n";
+  while(<$datFile>) {
+    if (/^(\S+):\s*/) {
+      $section=$1;
+      if ($section !~ m/^(logger|load|jks)$/) {
+        die "Invalid section in $inServerDatFile: $section\n";
+      }
+    }
+    if ($section eq "jks" and /password:\s+(\S+)\s*$/) {
+      $JKS_PASSWD=$1;
+    }
+    if ($section eq "jks" and /path:\s+(\S+)\s*$/) {
+      $JKS_PATH=$1;
+    }
+  }
+  close($datFile);
+  push @CLI, '# security realm config';
+  push @CLI, '/core-service=management/security-realm=UndertowRealm:add()';
+  push @CLI, '/core-service=management/security-realm=UndertowRealm/authentication=truststore:add(keystore-path="'.$JKS_PATH.'",keystore-password="'.$JKS_PASSWD.'")';
+  push @CLI, '/core-service=management/security-realm=UndertowRealm/server-identity=ssl:add(keystore-path="'.$JKS_PATH.'",keystore-password="'.$JKS_PASSWD.'")';
+}
+
 sub readServiceDatFile {
   my($serviceDatFile) = shift;
   my($datFile);
   my($name, $uri, $shortUri, $consumers);
+  my($servProto, $servHost, $servPort, $servResource);
+  my($urlOk) = 0;
   my @params = ();
   open($datFile, $serviceDatFile) || die "Cannot open $serviceDatFile\n";
   while(<$datFile>) {
@@ -118,15 +150,34 @@ sub readServiceDatFile {
       $shortUri=~s/^lwi\///;
       next;
     }
+    if (/\sbackEndServiceUrl:\s+(\w+):\/\/([^:]+):(\d+)(\/\S+)\s*$/) {
+      $servProto = lc($1);
+      $servHost = $2;
+      $servPort = $3;
+      $servResource = $4;
+      $urlOk = 1;
+    }
     if (/\s(\w+):\s+(\S+)\s*$/) {
       push @params, "\"$1\" => \"$2\"";
     }
   }
-  push @CLI, "/subsystem=undertow/server=default-server/host=default-host/location=\"/$uri\":add(handler=$name-pro)";
-  push @CLI, "/subsystem=undertow/configuration=handler/reverse-proxy=$name-pro:add(max-request-time=30000,connections-per-thread=2)";
-  push @CLI, "/subsystem=undertow/configuration=handler/reverse-proxy=$name-pro/host=$name-server1:add(outbound-socket-binding=$name-server,path='/LwiMockTargets/GetDocument',instance-id=$name-route1)";
+  if (!$urlOk) {
+    die "Service url syntax not recognized (proto://host:port/resource) in $serviceDatFile\n";
+  }
+  push @CLI, "/subsystem=undertow/server=default-server/host=default-host/location=\"/$uri\":add(handler=$name-proxy)";
+  push @CLI, "/subsystem=undertow/configuration=handler/reverse-proxy=$name-proxy:add(max-request-time=30000,connections-per-thread=2)";
+  if ($servProto eq "http") {
+    push @CLI, "/subsystem=undertow/configuration=handler/reverse-proxy=$name-proxy/host=${servHost}1:add(outbound-socket-binding=$servHost,path='$servResource',instance-id=$servHost-route1)";
+  } else {
+    push @CLI, "/subsystem=undertow/configuration=handler/reverse-proxy=$name-proxy/host=${servHost}1:add(scheme=$servProto,security-realm=UndertowRealm,outbound-socket-binding=$servHost,path='$servResource',instance-id=$servHost-route1)";
+  }
   push @CLI, "/subsystem=undertow/server=default-server/host=default-host/filter-ref=$name:add(predicate=path-prefix('/".$uri."'))";
   push @CLI, "/subsystem=undertow/configuration=filter/custom-filter=$name:add(class-name=hu.telekom.lwi.plugin.LwiHandler, module=hu.telekom.lwi,parameters = {".(join(',',@params))."})";
+ 
+  if (!exists($HOSTS{"$servHost:$servPort"})) {
+    push @CLI, "/socket-binding-group=standard-sockets/remote-destination-outbound-socket-binding=$servHost:add(host=$servHost, port=$servPort)";
+    $HOSTS{"$servHost:$servPort"}=1;
+  } 
   close($datFile);
 }
 
@@ -136,7 +187,7 @@ sub createAppUsersFile {
   open(my $propFile,">$outAppUsersFile") || die "Cannot write $outAppUsersFile\n";
   foreach my $userName (sort keys %USERS) {
       $md5cmd="echo $userName:$REALM:$USERS{$userName} | md5sum | cut -d' ' -f 1";
-      $md5=qx/$md5cmd/;
+      $md5=qx/$md5cmd/; 
       print $propFile "$userName=$md5";
   }
   close($propFile);
@@ -176,11 +227,12 @@ sub debugMsg {
 }
 
 sub printParams {
-  debugMsg("outAppRolesFile = $outAppRolesFile");
-  debugMsg("outAppUsersFile = $outAppUsersFile");
-  debugMsg("inUsersDatFile = $inUsersDatFile");
-  debugMsg("inServerDatFile = $inServerDatFile");
-  debugMsg("inServiceDatFileMask = $inServiceDatFileMask");
+  debugMsg("using the following parameter values:");
+  debugMsg("-appRoles = $outAppRolesFile");
+  debugMsg("-appUsers = $outAppUsersFile");
+  debugMsg("-userDat = $inUsersDatFile");
+  debugMsg("-serverDat = $inServerDatFile");
+  debugMsg("-serviceDatMask = $inServiceDatFileMask");
 }
 
 __END__
